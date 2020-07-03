35e492aae61fd91147f641a55b27a09e793a2ba1
monalto <64928009+monalto@users.noreply.github.com>
2020-07-04 00:34:46 +0200

    Zweiter Script Versuch

--- WPMT9.2_Torres.sh
+++ WPMT9.2_Torres.sh
@@ -1,6 +1,9 @@
  #!/bin/bash
 
 # Mit diesem Script werden die Spalten issn und Jahr bereinigt
-
-cat 2020-05-23-Article_list_dirty.tsv | awk -F="#" {print $1} | awk -F="IMPORTANT" {print $1} | awk -F="IMPORTANT!" {print $1} | awk -F="MAYBE" {print $1} | grep -P '\d{4}-\d{4}' issns.tsv > issns1.tsv | grep -P '\d{4}' issns.tsv > issns1.tsv | sort uniq issns1.tsv > issns_final.tsv
-
+cut -f 5,12 2020-05-23-Article_list_dirty.tsv > issns_and_years.tsv
+grep -v 'eng' issns_and_years.tsv > issns_and_years2.tsv
+sed 's/^[t\]*//' issns_and_years2.tsv > issns_and_years3.tsv
+sed 's/ISSN://g;s/issn://g;s/Issn://g' issns_and_years3.tsv > issns_and_years4.tsv
+sort -u issns_and_years4.tsv > issns_and_years5.tsv
+sort -n issns_and_years5.tsv > 2020-07-04-issns_and_years_clean.tsv
